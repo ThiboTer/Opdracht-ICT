cfc38258bdfaee1fab05648defb1f1133f0792f6
THIBO\terry <thibo.terryn@gmail.com>
2023-12-04 11:13:57 +0100

    Com poort selecteren

--- Opdracht_ICT/Opdracht_ICT/MainWindow.xaml.cs
+++ Opdracht_ICT/Opdracht_ICT/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,39 @@ namespace Opdracht_ICT
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort _serialPort;
         public MainWindow()
         {
             InitializeComponent();
+            _serialPort = new SerialPort();
+
+            cbxComPorts.Items.Add("None");
+            foreach (string s in SerialPort.GetPortNames())
+                cbxComPorts.Items.Add(s);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbxComPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                if (_serialPort.IsOpen)
+                    _serialPort.Close();
+
+                if (cbxComPorts.SelectedItem.ToString() != "None")
+                {
+                    _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
+                    _serialPort.Open();
+                }
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
